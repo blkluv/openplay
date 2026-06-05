b6e0b74658d1f7e3b4d3f9a0229d77e9d08ce3d6
WEB5D <info@blkluv.org>
2026-06-05 03:16:30 -0400

    Remove redundant comments in useUploadFile.ts

--- src/hooks/useUploadFile.ts
+++ src/hooks/useUploadFile.ts
@@ -17,10 +17,10 @@ export function useUploadFile(options: UploadFileOptions = {}) {
         throw new Error('Must be logged in to upload files');
       }
 
-+     // Check that the signer is ready and has the required methods
-+     if (!user.signer || typeof user.signer.signEvent !== 'function') {
-+       throw new Error('Signer not ready. Please unlock your extension or reload.');
-+     }
+      // Check that the signer is ready and has the required methods
+      if (!user.signer || typeof user.signer.signEvent !== 'function') {
+        throw new Error('Signer not ready. Please unlock your extension or reload.');
+      }
 
       const servers = options.servers || getEnabledServers();
 
