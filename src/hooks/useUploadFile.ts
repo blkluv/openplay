6d6f3be8e0d8580a51264bb3dd5a2a9d484074dc
The Wizard of Hahz <info@blkluv.org>
2026-06-06 06:08:52 -0400

    up

--- src/hooks/useUploadFile.ts
+++ src/hooks/useUploadFile.ts
@@ -1,5 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
-import { BlossomUploader } from '@nostrify/nostrify/uploaders';
+import { BlossomUploader } from "@nostrify/nostrify/uploaders";
 import { useCurrentUser } from "./useCurrentUser";
 import { useMediaServers } from "./useMediaServers";
 
@@ -14,23 +14,54 @@ export function useUploadFile(options: UploadFileOptions = {}) {
   return useMutation({
     mutationFn: async (file: File) => {
       if (!user) {
-        throw new Error('Must be logged in to upload files');
+        throw new Error("Must be logged in to upload files");
       }
 
-      // Check that the signer is ready and has the required methods
-      if (!user.signer || typeof user.signer.signEvent !== 'function') {
-        throw new Error('Signer not ready. Please unlock your extension or reload.');
+      if (!user.signer) {
+        throw new Error("Signer not found");
       }
+
+      console.log("========== UPLOAD DEBUG ==========");
+      console.log("User:", user);
+      console.log("Signer:", user.signer);
+      console.log("Signer methods:", Object.keys(user.signer || {}));
 
       const servers = options.servers || getEnabledServers();
+
+      console.log("Upload servers:", servers);
+      console.log("File:", {
+        name: file.name,
+        type: file.type,
+        size: file.size,
+      });
 
       const uploader = new BlossomUploader({
         servers,
         signer: user.signer,
       });
 
-      const tags = await uploader.upload(file);
-      return tags;
+      try {
+        console.log("Starting upload...");
+
+        const tags = await uploader.upload(file);
+
+        console.log("Upload successful");
+        console.log("Returned tags:", tags);
+
+        return tags;
+      } catch (err) {
+        console.error("UPLOAD ERROR:", err);
+        console.error("SERVERS:", servers);
+        console.error("SIGNER:", user.signer);
+        console.error("SIGNER METHODS:", Object.keys(user.signer || {}));
+
+        if (err instanceof Error) {
+          console.error("ERROR MESSAGE:", err.message);
+          console.error("ERROR STACK:", err.stack);
+        }
+
+        throw err;
+      }
     },
   });
 }
